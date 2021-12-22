4a3771cda66e49c55579214cdbc3f76f90f7cebc
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-22 12:05:43 +0500

    initial resources are ready

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios';
+
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL = 'https://vue-update-a4007-default-rtdb.asia-southeast1.firebasedatabase.app';
+axios.defaults.headers.common['Authorization'] = 'bla bla';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config);
+  return config;
+})
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res);
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
